36d9edc7b4b090aff49ae0671ca0129f56e1bf6d
Abdurrakhman <107718522+abdurahmanow@users.noreply.github.com>
2024-10-20 20:11:31 +0300

    V01.00.3

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 // Логика скрытия и показа меню при скролле
-let lastScrollTop = 0;
-const header = document.getElementById('main-header');
+///let lastScrollTop = 0;
+//const header = document.getElementById('main-header');
 
-window.addEventListener('scroll', function() {
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+//window.addEventListener('scroll', function() {
+ //   let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     
-    if (scrollTop > lastScrollTop && scrollTop > 300) {
-        header.style.top = '-100px';  // Скрыть
-    } else if (scrollTop < lastScrollTop) {
-        header.style.top = '0';  // Показать
-    }
+//    if (scrollTop > lastScrollTop && scrollTop > 300) {
+  //      header.style.top = '-100px';  // Скрыть
+  //  } else if (scrollTop < lastScrollTop) {
+ //       header.style.top = '0';  // Показать
+ //   }
     
-    lastScrollTop = scrollTop;
-});
+  //  lastScrollTop = scrollTop;
+//});
